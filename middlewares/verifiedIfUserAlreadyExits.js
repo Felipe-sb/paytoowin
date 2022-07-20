582cbebd9c9b5d8def7f4d1e1213b435ae0e05b9
Felipe-sb <felipe.soublette@gmail.com>
2022-07-20 13:28:41 -0400

    validate exits email

---
+++ middlewares/verifiedIfUserAlreadyExits.js
@@ -0,0 +1,21 @@
+const User = require('../models/User')
+const verifiedIfUserAlreadyExits = async (req,res,next) =>{
+    const {email} = req.body
+    const user = await User.find({email})
+    if (user || user.length !== 0) {
+        res.render('./authViews/register',{
+            alertConfig:{
+				alert:true,
+				title:`La cuenta con el correo ${email} ya existe`,
+				text:``,
+				icon:'warning',
+				confirmButton:true,
+				timer:false,
+				route:'auth/register'
+			}
+        })
+        return
+    }
+    next()
+}
+module.exports=verifiedIfUserAlreadyExits
