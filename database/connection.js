6ee07b20ba034d9951767803d325199352ed5ba6
Felipe-sb <felipe.soublette@gmail.com>
2022-09-29 22:22:35 -0300

    add config vars

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-connectionString = 'mongodb+srv://felipe_soublette:Felipe.2020@cluster0.bos6s.mongodb.net/paytoowin?retryWrites=true&w=majority'
+connectionString = process.env.CONNECTION_STRING_DB
 mongoose.connect(connectionString)
     .then(()=>{
         console.log('Connected to cluster');
