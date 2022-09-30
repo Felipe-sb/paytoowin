65dc7afd02c48df6bd23111c1e3a3b52bbf0f654
Felipe-sb <felipe.soublette@gmail.com>
2022-09-29 22:34:16 -0300

    modified navbar by rol

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-connectionString = process.env.CONNECTION_STRING_DB
+connectionString = process.env.CONNECTION_STRING_DB || 'mongodb+srv://felipe_soublette:Felipe.2020@cluster0.bos6s.mongodb.net/paytoowin?retryWrites=true'
 mongoose.connect(connectionString)
     .then(()=>{
         console.log('Connected to cluster');
