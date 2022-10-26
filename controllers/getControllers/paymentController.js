61b349f07cc18f4962f49e0f63768a7c6705af02
Felipe-sb <felipe.soublette@gmail.com>
2022-10-26 13:23:32 -0300

    fix paypal

--- controllers/getControllers/paymentController.js
+++ controllers/getControllers/paymentController.js
@@ -1,18 +1,22 @@
 const axios = require('axios');
 const User = require('../../models/User');
-const Product = require('../../models/Product')
+const Product = require('../../models/Product');
 const PAYPAL_API_URL = 'https://api-m.sandbox.paypal.com';
-const createOrder = async (req, res,next) => {
+const createOrder = async (req, res, next) => {
     if (req.session.loggedIn) {
-        const user = await User.findOne({email:req.session.email}).populate('cart')
-        const totalPrice = user.cart.map(product => product.price).reduce((before,now)=>before + now)
+        const user = await User.findOne({ email: req.session.email }).populate(
+            'cart'
+        );
+        const totalPrice = user.cart
+            .map((product) => product.price)
+            .reduce((before, now) => before + now);
         const order = {
             intent: 'CAPTURE',
             purchase_units: [
                 {
                     amount: {
                         currency_code: 'USD',
-                        value: `${totalPrice}`,
+                        value: `${totalPrice}}`,
                     },
                     description: 'accounts',
                 },
@@ -21,8 +25,8 @@ const createOrder = async (req, res,next) => {
                 brand_name: 'PayTooWin',
                 landing_page: 'NO_PREFERENCE',
                 user_action: 'PAY_NOW',
-                return_url: 'https://paytoowin.heokuapp.com/commerce/captureOrder',
-                cancel_url: 'https://paytoowin.herokuapp.com/commerce/cancelOrder',
+                return_url: 'http://localhost:4000/commerce/captureOrder',
+                cancel_url: 'http://localhost:4000/commerce/cancelOrder',
             },
         };
         const { data } = await axios.post(
@@ -31,41 +35,45 @@ const createOrder = async (req, res,next) => {
             {
                 auth: {
                     username:
-                        'AWCeTsS3yNzM5coeA0zkxBSg_Y6L_VNJASYqZqbH9y8fNSJmrvAbLUW9VbK83cnMj1_ToZBBjErBWlQ3',
+                        'AXUbtsveRCDN2gqJt-yhBabZNknmsDAuzcwdSOmomPOvrMSK9Vo0NQYf9CGZHh0ygxghRWAAG8wcgFs3',
                     password:
-                        'EACs7Nu33blg_OBgLJevnA7SBo5dcfKNKI7jfCYlPDAGrbk20ut5FlkLoRBZsEucl0PqICpxHaM0iXJh',
+                        'ENTgtGj36dghCJK58IWaNmoCM7QxoaiaIYNmTUQNj3eSf_nPa2Gl4-GGydZrO7vATIs2QZOM3zGUlz7k',
                 },
             }
         );
         res.redirect(`${data.links[1].href}`);
-        return
+        return;
     }
-    next()
+    next();
 };
 const cancelOrder = async (req, res) => {
     res.redirect('/commerce');
 };
 const captureOrder = async (req, res) => {
     const { token, PayerId } = req.query;
-    // const response = await axios.post(
-    //     `${PAYPAL_API_URL}/v2/checkout/orders/${token}/capture`,
-    //     {},
-    //     {
-    //         auth: {
-    //             username:
-    //                 'AWCeTsS3yNzM5coeA0zkxBSg_Y6L_VNJASYqZqbH9y8fNSJmrvAbLUW9VbK83cnMj1_ToZBBjErBWlQ3',
-    //             password:
-    //                 'EACs7Nu33blg_OBgLJevnA7SBo5dcfKNKI7jfCYlPDAGrbk20ut5FlkLoRBZsEucl0PqICpxHaM0iXJh',
-    //         },
-    //     }
-    // );
-    const user = await User.findOne({email:req.session.email}).populate('cart')
-    user.cart.forEach(async(product) =>{
-        await Product.findByIdAndUpdate(product.id,{partialdelete:true})
-    })
-    user.cart=[]
-    await user.save()
-    res.redirect('/commerce')
+    const response = await axios.post(
+        `${PAYPAL_API_URL}/v2/checkout/orders/${token}/capture`,
+        {},
+        {
+            auth: {
+                username:
+                    'AXUbtsveRCDN2gqJt-yhBabZNknmsDAuzcwdSOmomPOvrMSK9Vo0NQYf9CGZHh0ygxghRWAAG8wcgFs3',
+                password:
+                    'ENTgtGj36dghCJK58IWaNmoCM7QxoaiaIYNmTUQNj3eSf_nPa2Gl4-GGydZrO7vATIs2QZOM3zGUlz7k',
+            },
+        }
+    );
+    const user = await User.findOne({ email: req.session.email }).populate(
+        'cart'
+    );
+    user.cart.forEach(async (product) => {
+        await Product.findByIdAndUpdate(product.id, {
+            partialdelete: true,
+        });
+    });
+    user.cart = [];
+    await user.save();
+    res.redirect('/commerce');
 };
 module.exports = {
     createOrder,
