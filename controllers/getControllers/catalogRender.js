64ce1fb7d32d73767ee176c893acd9bad8189038
fabian ignacio escobar rojas <ignaciosapbe@MacBook-Air-Sapbe.local>
2022-10-27 06:59:07 -0300

    paginacion pag intermedias

--- controllers/getControllers/catalogRender.js
+++ controllers/getControllers/catalogRender.js
@@ -1,7 +1,7 @@
 const Product = require('../../models/Product');
 
 const getAllProducts = async (req, res, next) => {
-    let perPage = 2;
+    let perPage = 8;
     let page = req.params.page || 1;
 
 	if (req.session.loggedIn) {
@@ -23,6 +23,27 @@ const getAllProducts = async (req, res, next) => {
             });
         });
 	
-	}
+	} else {
+
+        Product.find({})
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec((err, products) => {
+                Product.count((err, count) => {
+                    if (err) return next(err);
+                    res.render('./productsViews/catalog', {
+                        products,
+                        current: page,
+                        pages: Math.ceil(count / perPage),
+                        login: null,
+                        msg: null
+                      
+                    });
+                    return
+                });
+            });
+        }
+        
+        
 };
 module.exports = getAllProducts;
