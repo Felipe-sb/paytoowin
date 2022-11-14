184258e1491368fb190f3c977d0f06a0da11c52f
fabian ignacio escobar rojas <ignaciosapbe@MacBook-Air-Sapbe.local>
2022-11-13 21:05:44 -0300

    envio de correo cuando cuenta es verificada

--- controllers/postControllers/verifiedProduct.js
+++ controllers/postControllers/verifiedProduct.js
@@ -1,6 +1,15 @@
 const Product = require("../../models/Product");
+const transporter = require('../../helpers/transporter');
+const User = require("../../models/User");
 const verifiedProduct = async(id)=>{
-const product = await Product.findByIdAndUpdate(id, {verified:true})
+const product = await Product.findByIdAndUpdate(id, {verified:true}).populate('owner')
+await transporter.sendMail({
+    from: '"paytoowin" <paytoowin.noreply@gmail.com>"',
+    to: `${product.owner[0].email}`,
+    subject: 'Cuenta verificada',
+    text: `Saludos ${product.owner[0].username} su cuenta ${product.username} fue verificada exitosamente `,
+});
+
 return product
 
 }
